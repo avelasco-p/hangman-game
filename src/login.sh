474059bd7aa2b32036a1e019867df4e0500c6a80
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-06-08 23:46:11 -0500

    changed login to use get_player function from crud script

--- src/login.sh
+++ src/login.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source ./crud.sh
 
 #database name hangman_db
 #password handled by pgpass file
@@ -6,7 +7,7 @@
 #arguments: (none)
 #------------------------------------------------------------------------------------------------------------------------------------------
 #variables:
-#	result: contains the tuple result of the psql query
+#	login_result: contains the tuple result of the psql query
 #return:
 #	0: query was successfull
 #	1: query wasnt successfull
@@ -16,9 +17,9 @@ function login() {
 	read -p "enter your nickname: " nickname
 	read -s -p "enter your passwod: " passwd
 	echo ""
-	login_result=$(psql -t -U postgres -d hangman_db -c "SELECT * FROM player WHERE nickname='$nickname' AND password=MD5('$passwd')")
+	local login_result=$(get_player $nickname $passwd)
 
-	if [[ -z $login_result ]]; then
+	if [[ $? == 1 ]]; then
 		return 1
 	else
 		return 0
