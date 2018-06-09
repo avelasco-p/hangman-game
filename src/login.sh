882f82968ee0809689edd894855553587e626652
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-06-08 22:09:50 -0500

    added docs and return value to login function, used in hangman.sh to check for credentials

--- src/login.sh
+++ src/login.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-database_name="hangman_db"
+#database name hangman_db
+#password handled by pgpass file
 
+#arguments: (none)
+#------------------------------------------------------------------------------------------------------------------------------------------
+#variables:
+#	result: contains the tuple result of the psql query
+#return:
+#	0: query was successfull
+#	1: query wasnt successfull
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
 function login() {
 	read -p "enter your nickname: " nickname
 	read -s -p "enter your passwod: " passwd
 	echo ""
-	result=$(psql -t -U postgres -d hangman_db -c "SELECT * FROM player WHERE nickname='$nickname' AND password=MD5('$passwd')")
+	login_result=$(psql -t -U postgres -d hangman_db -c "SELECT * FROM player WHERE nickname='$nickname' AND password=MD5('$passwd')")
+
+	if [[ -z $login_result ]]; then
+		return 1
+	else
+		return 0
+	fi
 }
