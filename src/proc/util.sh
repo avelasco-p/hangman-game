9550719039ed1c2680c438995ca2b4d7b0a55177
deviantfero <fmorataya.04@gmail.com>
2018-06-10 13:15:00 -0600

    fix typo

--- src/proc/util.sh
+++ src/proc/util.sh
@@ -2,7 +2,7 @@ function login_loop {
 	local nickname passwd user;
 	while [[ -z "$user" ]]; do
 		read -p "enter your nickname: " nickname;
-		read -s -p "enter your passwod: " passwd;
+		read -s -p "enter your password: " passwd;
 		user="$(get_player "$nickname" "$passwd")";
 	done
 	echo "$user";
